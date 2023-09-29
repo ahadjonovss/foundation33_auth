25c935dc465fb18a19b388372f1275efb4c1cb00
ahadjonovss <samandarahadjonov@gmail.com>
2023-09-29 11:31:11 +0500

    currency

---
+++ data.py
@@ -0,0 +1,26 @@
+import requests
+
+
+
+class Currency:
+    def __init__(self, json):
+        self.title = json["title"]
+        self.code = json["code"]
+        self.cb_price = json["cb_price"]
+        self.nbu_buy_price = json["nbu_buy_price"]
+        self.nbu_cell_price = json["nbu_cell_price"]
+        self.date = json["date"]
+
+
+def getData():
+    url = "https://nbu.uz/uz/exchange-rates/json/"
+    response = requests.get(url)
+    print(response)
+    usd_price = None
+    currencies = []
+    if response.status_code == 200:
+        data = response.json()
+        for i in data:
+            currencies.append(Currency(i))
+
+    return currencies
